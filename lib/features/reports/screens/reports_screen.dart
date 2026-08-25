c70d4fdecf5809240fe265cb84644e91c0187172
ardhikaxx <ardhikayanuar58@gmail.com>
2026-08-25 16:35:43 +0700

    fix(reports): perapian kode dan impor pada reports_screen.dart

--- lib/features/reports/screens/reports_screen.dart
+++ lib/features/reports/screens/reports_screen.dart
@@ -4,7 +4,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../config/gate.dart';
-import '../../../config/providers.dart';
 import '../../../core/theme/app_theme.dart';
 import '../../../core/utils/formatters.dart';
 import '../../../core/widgets/common.dart';
