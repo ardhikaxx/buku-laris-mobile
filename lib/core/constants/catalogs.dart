9bdba880835deb13da326c7df1d5ce65c0e35e6a
ardhikaxx <ardhikayanuar58@gmail.com>
2026-08-25 11:25:16 +0700

    feat(core): penambahan data katalog dan kategori bawaan pada catalogs.dart

---
+++ lib/core/constants/catalogs.dart
@@ -0,0 +1,50 @@
+class CashCategories {
+  CashCategories._();
+
+  static const income = [
+    'Penjualan',
+    'Pembayaran DP',
+    'Pelunasan Piutang',
+    'Modal Masuk',
+    'Pendapatan Lain',
+  ];
+
+  static const expense = [
+    'Pembelian Stok',
+    'Biaya Operasional',
+    'Transportasi',
+    'Listrik & Air',
+    'Internet',
+    'Gaji Karyawan',
+    'Sewa Tempat',
+    'Marketing',
+    'Biaya Lainnya',
+  ];
+}
+
+class BusinessCatalogs {
+  BusinessCatalogs._();
+
+  static const businessTypes = [
+    'Retail / Toko',
+    'Kuliner & Katering',
+    'Fashion',
+    'Elektronik & Gadget',
+    'Servis & Bengkel',
+    'Laundry',
+    'Salon & Kecantikan',
+    'Desain & Kreatif',
+    'Produk Digital',
+    'Fotografi',
+    'Pertanian & Peternakan',
+    'Lainnya',
+  ];
+
+  static const businessScales = ['Mikro', 'Kecil', 'Menengah'];
+
+  static const timezones = [
+    ('Asia/Jakarta', 'WIB - Jakarta'),
+    ('Asia/Makassar', 'WITA - Makassar'),
+    ('Asia/Jayapura', 'WIT - Jayapura'),
+  ];
+}
