b16745870d14d89ee5424ff8893439c04c8d0c6f
ardhikaxx <ardhikayanuar58@gmail.com>
2026-08-25 21:23:06 +0700

    style: bersihkan lint app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -15,7 +14,7 @@ class BukuLarisApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    ref.listen(gateProvider.select((s) => s.status), (_, __) {});
+    ref.listen(gateProvider.select((s) => s.status), (_, _) {});
     FlutterError.onError = (details) {
       Logger.e('FLUTTER ERROR: ${details.exception}', details.stack);
       FlutterError.presentError(details);
